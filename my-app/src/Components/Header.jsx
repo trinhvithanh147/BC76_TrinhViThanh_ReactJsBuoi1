15481c2efc024e0c764f38d22a59739e65ebfe02
trinhvithanh147 <trinhvidanhthanh@gmail.com>
2024-10-13 06:23:02 +0700

    Chinh lai responsive

--- my-app/src/Components/Header.jsx
+++ my-app/src/Components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className='py-12 header overflow-hidden'>
         <div className="container lg:px-12">
             <div className="header_content p-6 lg:p-12 text-center rounded-[0.5rem]">
-                <div className="m-7 lg:m-12">
+                <div className="m-8 lg:m-12">
                     <h1 className='font-bold lg:text-5xl text-3xl mb-2'>A warm welcome!</h1>
                     <p className='fs-4  mb-4'>Bootstrap utility classes are used to create this jumbotron since the old 
                         component has been removed from the framework. Why create custom CSS when you can use utilities?</p>
