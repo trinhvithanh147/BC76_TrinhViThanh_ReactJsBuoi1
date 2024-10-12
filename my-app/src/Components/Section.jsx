aed5845ff3324e2e3e3cdd3028d44cc1844fce68
trinhvithanh147 <trinhvidanhthanh@gmail.com>
2024-10-13 06:44:26 +0700

    hoan thanh

--- my-app/src/Components/Section.jsx
+++ my-app/src/Components/Section.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Card from './Card'
 const Section = () => {
     return (
-        <div className='pt-6 section'>
+        <div className='pt-6 section overflow-hidden'>
             <div className="container lg:px-12">
                     <Card/>
             </div>
